09c03953d23d4f2137110acca32dbc92d2daea29
dnns01 <github@dnns01.de>
2021-11-09 22:16:16 +0100

    Update strolchguru and clipupdater to download clips now.

--- clip_updater.py
+++ clip_updater.py
@@ -1,7 +1,62 @@
 import sqlite3
 import time
+from pprint import pprint
 
-import requests
+import youtube_dl
+from gql import Client, gql
+from gql.transport.requests import RequestsHTTPTransport
+
+GQL_query_clips = """
+query($after: Cursor) {
+    user(login: "indiestrolche") {
+        clips(first: 100, after: $after, criteria: { 
+            sort: VIEWS_DESC, 
+            period: ALL_TIME 
+        }) {
+            edges {
+                node {
+                    title
+                    id
+                    url
+                    embedURL
+                    slug
+                    thumbnailURL(width: 480, height: 272)
+                    curator {
+                        displayName
+                    }
+                    createdAt
+                    durationSeconds
+                }
+                cursor
+            }
+        }
+    }
+}
+"""
+
+
+def get_clip(cursor, clip_id: int):
+    cursor.execute('SELECT id, title, clip_id, url, embed_url, slug, thumbnail_url, curator, clip_url, is_published, '
+                   'created_at, custom_title, duration, is_downloaded FROM strolchguru_clip '
+                   'WHERE clip_id = ?', (clip_id,))
+    clip = cursor.fetchone()
+
+    return None if not clip else {
+        "id": clip[0],
+        "title": clip[1],
+        "clip_id": clip[2],
+        "url": clip[3],
+        "embed_url": clip[4],
+        "slug": clip[5],
+        "thumbnail_url": clip[6],
+        "curator": clip[7],
+        "clip_url": clip[8],
+        "is_published": clip[9],
+        "created_ad": clip[10],
+        "custom_title": clip[11],
+        "duration": clip[12],
+        "is_downloaded": clip[13]
+    }
 
 
 def save_clips(clips):
@@ -18,30 +73,55 @@ def save_clips(clips):
             thumbnail_url = node["thumbnailURL"]
             curator = node["curator"]["displayName"] if node["curator"] else "Frag mich mal was leichteres"
             clip_url = thumbnail_url.split("-preview-")[0] + ".mp4"
+            created_at = node["createdAt"]
+            duration = node["durationSeconds"]
 
-            c.execute(
-                'INSERT INTO strolchguru_clip (title, clip_id, url, embed_url, slug, thumbnail_url, curator, clip_url, is_published) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?) ON CONFLICT (clip_id) DO NOTHING',
-                (title, clip_id, url, embed_url, slug, thumbnail_url, curator, clip_url, True))
+            if clip_id == 946839771:
+                pprint(node)
+
+            if clip := get_clip(c, clip_id):
+                c.execute('UPDATE strolchguru_clip SET title = ?, url = ?, embed_url = ?, slug = ?, thumbnail_url = ?, '
+                          'curator = ?, clip_url = ?, is_published = ?, created_at = ?, duration = ? WHERE clip_id = ?',
+                          (title, url, embed_url, slug, thumbnail_url, curator, clip_url, True, created_at, duration,
+                           clip_id))
+            else:
+                c.execute(
+                    'INSERT INTO strolchguru_clip (title, clip_id, url, embed_url, slug, thumbnail_url, curator, '
+                    'clip_url, is_published, created_at, duration, is_downloaded) '
+                    'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                    (title, clip_id, url, embed_url, slug, thumbnail_url, curator, clip_url, True, created_at, duration,
+                     False))
 
     conn.commit()
     conn.close()
 
 
-def get_clips(cursor=None):
-    auth = "kimne78kx3ncx6brgo4mv6wki5h1ko"
-    headers = {"client-id": f"{auth}", "Content-Type": "application/json"}
-    after = f", after: \"{cursor}\"" if cursor else ""
-    response = requests.post("https://gql.twitch.tv/gql",
-                             headers=headers,
-                             json={
-                                 "query": "query {\r\n user(login: \"indiestrolche\") {\r\n clips(first: 100" + after + ", criteria: { sort: VIEWS_DESC, period: ALL_TIME }) {\r\n edges {\r\n node {\r\n title\r\n id\r\n url\r\n embedURL\r\n slug\r\n thumbnailURL\r\n \t\t\t\t\tcurator {\r\n displayName\r\n }\r\n }\r\n cursor\r\n }\r\n }\r\n }\r\n}"})
+def get_client() -> Client:
+    transport = RequestsHTTPTransport(
+        url="https://gql.twitch.tv/gql",
+        verify=True,
+        retries=3,
+        headers={
+            'Content-Type': 'text/plain;charset=UTF-8',
+            'Client-ID': 'kimne78kx3ncx6brgo4mv6wki5h1ko',
+        }
+    )
 
-    if response.status_code == 200:
-        json = response.json()
-        if json.get("errors"):
-            return
-        if data := json.get("data"):
-            return data["user"]["clips"]["edges"]
+    return Client(
+        transport=transport,
+        fetch_schema_from_transport=True
+    )
+
+
+def get_clips(cursor: str = "") -> dict:
+    query = gql(GQL_query_clips)
+    client = get_client()
+    try:
+        result = client.execute(query, variable_values={"after": cursor})
+    except Exception:
+        return get_clips(cursor=cursor)
+
+    return result["user"]["clips"]["edges"]
 
 
 def get_all_clips():
@@ -56,7 +136,28 @@ def get_all_clips():
         time.sleep(10)
 
 
+def download_clips():
+    conn = sqlite3.connect("db.sqlite3")
+    c = conn.cursor()
+
+    c.execute("SELECT clip_id, url FROM strolchguru_clip WHERE is_downloaded IS false")
+    clips = c.fetchall()
+
+    with youtube_dl.YoutubeDL({"writethumbnail": True, "outtmpl": "clips/%(id)s.%(ext)s"}) as ytdl:
+        for clip in clips:
+            try:
+                ytdl.download([clip[1]])
+                c.execute("UPDATE strolchguru_clip SET is_downloaded = true WHERE clip_id = ? ", (clip[0],))
+                conn.commit()
+            except:
+                pass
+
+    conn.close()
+
+
 while True:
     get_all_clips()
-    print("Sooo... alle Clips wurden gegönnt :)")
+    print("Sooo... alle Clips wurden gegönnt... starte mit dem Download")
+    download_clips()
+    print("Auch alle Clips downgeloaded")
     time.sleep(1800)
--- strolchguru/models.py
+++ strolchguru/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Clip(models.Model):
     title = models.CharField(max_length=100)
+    custom_title = models.CharField(max_length=100, null=True)
     clip_id = models.IntegerField(unique=True)
     url = models.URLField()
     embed_url = models.URLField()
@@ -10,4 +11,7 @@ class Clip(models.Model):
     thumbnail_url = models.URLField()
     curator = models.CharField(max_length=25)
     clip_url = models.URLField()
+    duration = models.IntegerField(default=0)
+    created_at = models.DateTimeField()
     is_published = models.BooleanField(default=True)
+    is_downloaded = models.BooleanField(default=False)
