003345018cbe55c4c8ba1f5e35550c9e33a6e003
dnns01 <github@dnns01.de>
2021-11-14 21:18:56 +0100

    Added pagination to clips

--- strolchguru/views.py
+++ strolchguru/views.py
@@ -1,4 +1,5 @@
 import random
+import re
 
 from django.core.paginator import Paginator
 from django.db.models import Q
@@ -9,6 +10,24 @@ from django.shortcuts import render, get_object_or_404
 from strolchibot.models import TwitchUser
 from .forms import ClipSearchForm
 from .models import Clip
+
+
+def get_pages(page, num_pages):
+    if page > num_pages:
+        raise Http404
+
+    if num_pages <= 5:
+        return range(1, num_pages + 1)
+    else:
+        start_page = page - 2 if page >= 3 else 1
+        end_page = page + 2 if page <= num_pages - 2 else num_pages
+
+        if end_page - start_page < 4:
+            if start_page < 3:
+                end_page = start_page + 4
+            elif end_page > num_pages - 2:
+                start_page = end_page - 4
+        return range(start_page, end_page + 1)
 
 
 def home(request) -> HttpResponse:
@@ -39,21 +58,28 @@ def clip_json(request, id) -> JsonResponse:
 
 
 def clips(request) -> HttpResponse:
+    page = request.GET.get("page", "1")
     clips = Clip.objects.filter(is_downloaded=True).order_by("-created_at")
     if not (isinstance(request.user, TwitchUser) and request.user.is_mod and request.user.is_admin):
         clips = clips.filter(is_published=True)
 
     form = ClipSearchForm(request.GET)
-    # check whether it's valid:
     if form.is_valid():
         search = form.cleaned_data["search"]
         clips = clips.filter(
-            Q(title__icontains=search) | Q(curator__icontains=search) | Q(tags__name__icontains=search))
+            Q(title__icontains=search) | Q(curator__icontains=search) | Q(tags__name__icontains=search)).distinct()
 
     else:
         clips = None
 
-    paginator = Paginator(clips, 50)
-    page_obj = paginator.get_page(1)
+    if re.match("^[0-9]+$", page):
+        page = int(page)
+    else:
+        raise Http404
 
-    return render(request, "clips.html", context={"title": "Clips", "clips": page_obj, "search_form": form})
+    paginator = Paginator(clips, 50)
+    pages = get_pages(page, paginator.num_pages)
+    page_obj = paginator.get_page(page)
+
+    return render(request, "clips.html",
+                  context={"title": "Clips", "clips": page_obj, "search_form": form, "page": page, "pages": pages})
--- strolchibot/static/js/scripts.js
+++ strolchibot/static/js/scripts.js
@@ -1,7 +1,7 @@
 document.querySelectorAll(".tablink").forEach(value => {
     value.addEventListener("click", evt => {
-        var i, x, tablinks;
-        var cityName = value.getAttribute("id").replace("tablink_", "");
+        let i, x, tablinks;
+        const cityName = value.getAttribute("id").replace("tablink_", "");
         x = document.getElementsByClassName("form");
         for (i = 0; i < x.length; i++) {
             x[i].style.display = "none";
@@ -18,10 +18,35 @@ document.querySelectorAll(".tablink").forEach(value => {
 
 document.querySelectorAll(".accordeon").forEach(value => {
     value.addEventListener("click", evt => {
-        var accordeonItem = document.querySelector("#" + value.id + "-element");
+        const accordeonItem = document.querySelector("#" + value.id + "-element");
         accordeonItem.classList.toggle("w3-show");
         accordeonItem.classList.toggle("w3-hide");
         evt.target.classList.toggle("fa-chevron-down");
         evt.target.classList.toggle("fa-chevron-up");
     });
 });
+
+document.querySelectorAll("#pagination a").forEach(value => {
+    let pathname = window.location.pathname
+    let urlParams = new URLSearchParams(window.location.search)
+    let currentPage = getCurrentPage(urlParams)
+    if (value.id === "prev") {
+        urlParams.set("page", (currentPage - 1).toString());
+    } else if (value.id === "next") {
+        urlParams.set("page", (currentPage + 1).toString());
+    } else {
+        urlParams.set("page", value.textContent)
+    }
+
+    value.setAttribute("href", pathname + "?" + urlParams.toString())
+})
+
+function getCurrentPage(urlParams) {
+    let currentPage = urlParams.get("page")
+
+    if (!currentPage) {
+        return 1;
+    } else {
+        return parseInt(currentPage);
+    }
+}
